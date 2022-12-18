d8fd332ad361d56c47cab51eb845ebac81737613
AdityaKG-169 <aditya.kg169@gmail.com>
2022-12-18 16:08:43 +0530

    fix: Helpeer popup images made non selectable and non draggable

--- react-app/src/Components/SearchPrompt/SearchPrompt.tsx
+++ react-app/src/Components/SearchPrompt/SearchPrompt.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+
+import './SearchPrompt.scss';
+
 const SearchPrompt = () => {
+	useEffect(() => {}, []);
+
 	return (
-		<div>
-			<h1>SearchPrompt</h1>
+		<div className="search_prompt">
+			<button>Login</button>
 		</div>
 	);
 };

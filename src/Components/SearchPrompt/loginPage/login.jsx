f99d79e1cec6e3471646efdc4b86358c9f5ee342
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2023-03-09 18:58:31 +0530

    Changed class names to avoid overriding

--- src/Components/SearchPrompt/loginPage/login.jsx
+++ src/Components/SearchPrompt/loginPage/login.jsx
@@ -9,10 +9,10 @@ const LoginPage = () => {
 
     return (
         <div class="sheldon__content">
-            <img class="icon" src={chrome.runtime.getURL('assets/icons/icon48.png')} />
-            <div class="heading">Welcome to Sheldon</div>
-            <p class="text">AI Assistant that Saves 80% of your Time on the Internet</p>
-            <button className='login-button' onClick={handleClick}>
+            <img class="sheldon__icon" src={chrome.runtime.getURL('assets/icons/icon48.png')} />
+            <div class="headingg">Welcome to Sheldon</div>
+            <p class="textt">AI Assistant that Saves 80% of your Time on the Internet</p>
+            <button className='login-buttonn' onClick={handleClick}>
                <img className='google-img' src={chrome.runtime.getURL('assets/icons/google.svg')} /> Continue with Google
                 </button>
             <img className='messy-imgg' src={chrome.runtime.getURL('assets/icons/unboxing.svg')} />  
